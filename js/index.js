79863fa533cf461c09789b8c1da29fe639851233
Toosdai Otte <Toosdai52Otte@gmail.com>
2019-06-10 23:44:41 -0500

    finished MVP

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
 // Your code goes here
 
-// 1. MouseOver on .btn
+// 1. Change color when Clicked
+const colorChange = document.querySelectorAll('.destination');
+
+colorChange[0].addEventListener('click', (event) => {
+         event.target.setAttribute('style', 'color: yellow');
+         setTimeout(function(){
+            event.target.setAttribute('style', 'color: black');  // Change the color back to the original
+          }, 3000); 
+    });
+
+colorChange[1].addEventListener('click', (event) => {
+    event.target.setAttribute('style', 'color: forestgreen');
+    setTimeout(function(){
+        event.target.setAttribute('style', 'color: black');  // Change the color back to the original
+      }, 3000); 
+});
+
+colorChange[2].addEventListener('click', (event) => {
+    event.target.setAttribute('style', 'color: #17A2B8');
+    setTimeout(function(){
+        event.target.setAttribute('style', 'color: black');  // Change the color back to the original
+      }, 3000); 
+});
+
+// 2. MouseOver on .btn
 const bttn = document.querySelectorAll('.btn');
 
 bttn.forEach((index) => {
@@ -8,14 +32,19 @@ bttn.forEach((index) => {
     event.target.style.fontSize = '3rem';
     });
 
-// 2. MouseOut on .btn  
+// 3. MouseOut on .btn  
     index.addEventListener('mouseout', (event) => {
         event.target.style.fontSize = "";
     });
     
+// Stop Propagation on button text
+    index.addEventListener('click', (event) => {
+        event.target.style.color = 'black';
+        event.stopPropagation();
+    });
 });
 
-// 3. Rounded Imgs when DoubleClicked
+// 4. Rounded Imgs when DoubleClicked
 const middleImgs = document.querySelectorAll('.img-content');
 middleImgs.forEach((index) => { 
     index.addEventListener('dblclick', (event) => {
@@ -23,34 +52,11 @@ middleImgs.forEach((index) => {
     });
 });
 
-// 4. KeyDown Alert Box
-
+// 5. KeyDown Alert Box
 const body = document.querySelector('body');
 body.addEventListener('keydown', () => {
     alert('Nothing selected!')
 });
-
-// 5. Change h4 color when Clicked
-
-const colorChange = document.querySelectorAll('.destination');
-
-
-
-colorChange[0].addEventListener('click', (event) => {
-        event.target.setAttribute('style', 'color: yellow');  
-    });
-
-colorChange[1].addEventListener('click', (event) => {
-    event.target.setAttribute('style', 'color: forestgreen');
-});
-
-colorChange[2].addEventListener('click', (event) => {
-    event.target.setAttribute('style', 'color: #17A2B8');
-    
-});
-
-// stopPropagation()
-
 
 // 6. Alert box when Scrolled to bottom
 window.addEventListener('scroll', () => {
@@ -61,10 +67,30 @@ window.addEventListener('scroll', () => {
     }
 });
 
-// 7. 
+// 7. Yellow Background when Copied
+const pTags = document.querySelectorAll('p');
+pTags.forEach((index) => {
+    index.addEventListener('copy', (event) => {
+        event.target.style.color = 'yellow';
+    });
+});
 
+// // 8. MouseEnter on .main-navigation
+const mainNav = document.querySelector('.main-navigation');
+mainNav.addEventListener('mouseenter', (event) => {
+	event.target.setAttribute('style', 'border-bottom: solid #C0C0C0');
+});
 
+// 9. MouseLeave on .main-navigation
+mainNav.addEventListener('mouseleave', (event) => {
+    event.target.setAttribute('style', 'border-bottom: dashed #C0C0C0');
+});
 
+// 10. Prevent Default context menu on .intro Img
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('contextmenu', (event) => {
+	event.preventDefault();
+});
 
 // preventDefault() .nav
 const nav = document.querySelector('.nav');
